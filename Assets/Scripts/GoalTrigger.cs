26bc4e44cc7ef2a81e20544b449ae0d6f943a133
Ryo Hashimoto <register.ryohu@gmail.com>
2019-01-22 12:02:51 +0900

    'init'

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private ParticleSystem system;
+    private AudioSource audio;
+    private bool isGoal = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = GetComponent<ParticleSystem>();
+        audio = GetComponent<AudioSource>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isGoal && other.tag == "Player")
+        {
+            isGoal = true;
+            system.Play();
+            audio.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/screenshot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenshot : MonoBehaviour
+{
+    [SerializeField] string name = "Sample.png";
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScreenCapture.CaptureScreenshot(Application.dataPath + "/" + name);
+    }
+}
